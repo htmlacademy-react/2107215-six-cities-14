ed60383e7eeeb103c2b8699ba4aaaea7b78759ed
Alena59 <asdalena59@gmail.com>
2023-10-17 20:51:27 +0300

    css

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -2,42 +2,12 @@ import {Link} from 'react-router-dom';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <section className="no-page"
-      style={{
-        width: '100vw',
-        height: '100%',
-        background: '#b8dce8',
-        position: 'absolute'
-      }}
-    >
+    <section className="no-page">
       <h1 className="visually-hidden">404. Page not found</h1>
-      <img className="no-page__image" src="img/not-found-bg.jpg" alt="Not found page image"
-        style={{
-          width: '100%',
-          maxHeight: '1100px',
-          objectFit: 'contain'
-        }}
-      />
-      <div className="no-page__content"
-        style={{
-          position: 'absolute',
-          fontSize: '28px',
-          top: '10%',
-          left: '12%',
-          padding: '30px'
-        }}
-      >
-        <div className="no-page__text"
-          style={{
-            paddingBottom: '30px'
-          }}
-        >
-          <img className="no-page__image" src="img/oops-bg.png" width={500} height={60} alt="Not found page image"
-            style={{
-              position: 'relative',
-              left: '-8%'
-            }}
-          />
+      <img className="no-page__image" src="img/not-found-bg.jpg" alt="Not found page image" />
+      <div className="no-page__content">
+        <div className="no-page__text">
+          <img className="no-page__image-text" src="img/oops-bg.png" width={500} height={60} alt="Not found page image" />
           <p>Where are we?</p>
           <p>The page you are lookiing for was moved,<br/> removed, renamed or might never existed.</p>
         </div>
