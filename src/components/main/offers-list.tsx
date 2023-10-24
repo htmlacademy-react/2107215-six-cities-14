f19d2b8d01c8a108f69805c999ad91d5e70b4c7a
Alena59 <asdalena59@gmail.com>
2023-10-24 19:43:08 +0300

    offers

--- src/components/main/offers-list.tsx
+++ src/components/main/offers-list.tsx
@@ -1,30 +1,12 @@
-import PlaceCard from '../ui/offer-card';
 import {Offer} from '../../index/index';
+import Offers from './offers';
 
-type PlacesProps = {
+type OffersProps = {
   offers: Offer[];
 }
 
-function Offers({offers}: PlacesProps): (JSX.Element | null)[] | null {
-  return (
-    offers.map((item: Offer): JSX.Element => {
-      const {images} = item;
-      return (
-        <article key={item.id} className="cities__card place-card">
-          <div className="cities__image-wrapper place-card__image-wrapper">
-            <a href="#">
-              {images && <img className="place-card__image" src={images[0]} width={260} height={200} alt="Place image"/>}
-            </a>
-          </div>
-          <PlaceCard offer={item}/>
-        </article>
-      );
-    })
-  );
-}
-
-function OffersList({offers}: PlacesProps): JSX.Element|null {
-  return offers?.length ?
+function OffersList({offers}: OffersProps): JSX.Element|null {
+  return offers.length ?
     <div className="cities__places-list places__list tabs__content">
       <Offers offers={offers} />
     </div>
---
+++ src/components/main/offers.tsx
@@ -0,0 +1,26 @@
+import OfferCard from '../ui/offer-card';
+import {Offer} from '../../index/index';
+
+type OffersProps = {
+  offers: Offer[];
+}
+
+function Offers({offers}: OffersProps): (JSX.Element | null)[] | null {
+  return (
+    offers.map((item: Offer): JSX.Element => {
+      const {images} = item;
+      return (
+        <article key={item.id} className="cities__card place-card">
+          <div className="cities__image-wrapper place-card__image-wrapper">
+            <a href="#">
+              {images && <img className="place-card__image" src={images[0]} width={260} height={200} alt="Place image"/>}
+            </a>
+          </div>
+          <OfferCard offer={item}/>
+        </article>
+      );
+    })
+  );
+}
+
+export default Offers;
