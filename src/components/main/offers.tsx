338c23fd9637724d88f2599d550088bd7fc542a5
Alena59 <asdalena59@gmail.com>
2023-10-26 14:06:07 +0300

    доработки

--- src/components/main/offers.tsx
+++ src/components/main/offers.tsx
@@ -3,14 +3,21 @@ import {Offer} from '../../types/index';
 
 type OffersProps = {
   offers: Offer[];
+  onOfferMouseOver: (id: number) => void;
+  onOfferMouseLeave: () => void;
 }
 
-function Offers({offers}: OffersProps): (JSX.Element | null)[] | null {
+function Offers({offers, onOfferMouseOver, onOfferMouseLeave} : OffersProps) {
+
   return (
     offers.map((item: Offer): JSX.Element => {
-      const {images} = item;
+      const {images, id} = item;
       return (
-        <article key={item.id} className="cities__card place-card">
+        <article key={item.id}
+          className="cities__card place-card"
+          onMouseLeave={onOfferMouseLeave}
+          onMouseOver={() => onOfferMouseOver(id)}
+        >
           <div className="cities__image-wrapper place-card__image-wrapper">
             <a href="#">
               {images && <img className="place-card__image" src={images[0]} width={260} height={200} alt="Place image"/>}
